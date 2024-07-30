dd251d61b8ae9b5aa96c440dd69dc0df28052ff7
hpqztsc <49110606+hpqztsc@users.noreply.github.com>
2024-07-30 16:57:17 +0800

    Update utils.go
    used beijing time

--- server/tool/utils.go
+++ server/tool/utils.go
@@ -95,7 +95,12 @@ func getSeverStatus() {
 			m["io_send"] = (io2[0].BytesSent - io1[0].BytesSent) * 2
 			m["io_recv"] = (io2[0].BytesRecv - io1[0].BytesRecv) * 2
 		}
-		t := time.Now()
+		loc, err := time.LoadLocation("Asia/Shanghai")
+	        if err != nil {
+		        fmt.Println("Error loading location:", err)
+		         return
+	        }
+		t := time.Now().In(loc)
 		m["time"] = strconv.Itoa(t.Hour()) + ":" + strconv.Itoa(t.Minute()) + ":" + strconv.Itoa(t.Second())
 
 		for _, v := range conn {
